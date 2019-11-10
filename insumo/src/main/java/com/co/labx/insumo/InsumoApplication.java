a63b00f2ec2e1c31b1970f207e29802bcde25789
Julian Galeano <judres03@gmail.com>
2019-11-10 00:34:42 -0500

    FIx Cors
    FIx Cors

--- insumo/src/main/java/com/co/labx/insumo/InsumoApplication.java
+++ insumo/src/main/java/com/co/labx/insumo/InsumoApplication.java
@@ -2,30 +2,14 @@ package com.co.labx.insumo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.co.labx.insumo.security.AuthInterceptor;
-
-@SuppressWarnings("deprecation")
 @SpringBootApplication
 @EnableWebMvc
-public class InsumoApplication extends WebMvcConfigurerAdapter {
+public class InsumoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsumoApplication.class, args);
-	}
-
-	@Bean
-	public AuthInterceptor authInterceptor() {
-	    return new AuthInterceptor();
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(authInterceptor()).addPathPatterns("/**");
 	}
 
 }
